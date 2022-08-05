30393bc25858592bf873013d2e47d41e638ea3ee
Vladislav Surin <vlad_surin@mail.ru>
2022-08-05 12:35:04 +0300

    finished third task

---
+++ homework-1/task-3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+using System;
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int userNum = Prompt("Введите число > ");
+
+if (userNum % 2 == 0) {
+    Console.WriteLine("Число четное");
+} else {
+    Console.WriteLine("Число нечетное");
+}
