9954a562612f173ac32e35c1f63207f6590edb4b
Vladislav Surin <vlad_surin@mail.ru>
2022-08-05 12:08:57 +0300

    finished second task

--- homework-1/task-1/Program.cs
+++ homework-1/task-1/Program.cs
@@ -1,18 +1,26 @@
-﻿int Prompt(string message)
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+using System;
+
+int Prompt(string message)
 {
-    System.Console.Write(message);          // Вывести сообщение
-    string readValue = Console.ReadLine();  // Считавает с консоли строку
-    int result = int.Parse(readValue);      // Преобрадует строку в целое число
-    return result;                          // Возвращает результат
+    Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
 }
 
 int userNum1 = Prompt("Введите первое число > ");
 int userNum2 = Prompt("Введите второе число > ");
 
 if (userNum1 > userNum2) {
-    System.Console.WriteLine($"Число {userNum1} больше числа {userNum2}");
+    Console.WriteLine($"max = {userNum1} min = {userNum2}");
 } else if (userNum1 < userNum2) {
-    System.Console.WriteLine($"Число {userNum1} меньше числа {userNum2}");
+    Console.WriteLine($"max = {userNum2} min = {userNum1}");
 } else if (userNum1 == userNum2) {
-    System.Console.WriteLine($"Число {userNum1} равно числу {userNum2}");
+    Console.WriteLine($"{userNum1} = {userNum2}");
 }
---
+++ homework-1/task-2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+using System;
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int userNum1 = Prompt("Введите первое число > ");
+int userNum2 = Prompt("Введите второе число > ");
+int userNum3 = Prompt("Введите третье число > ");
+int maxNum = userNum1;
+
+if (userNum2 > maxNum) {
+    maxNum = userNum2;
+} else if (userNum3 > maxNum) {
+    maxNum = userNum3;
+}
+
+Console.WriteLine(maxNum);
