ef7bbea962f83d1d2ca4b01961d458630db76a7d
Vladislav Surin <vlad_surin@mail.ru>
2022-08-09 13:34:02 +0300

    finished fourth task

---
+++ homework-2/task-3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int userNum = Prompt("Введите число > ");
+Console.WriteLine(userNum);
---
+++ homework-2/task-4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int day = Prompt("Введите номер дня недели > ");
+
+int[] week = { 0, 0, 0, 0, 0, 1, 1 };
+
+if (week[day - 1] == 1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
