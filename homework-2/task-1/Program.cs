6c88da560d5faa66a3e754cad5880bae696547a9
Vladislav Surin <vlad_surin@mail.ru>
2022-08-09 12:36:41 +0300

    finished first task

---
+++ homework-2/task-1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+int userNum = Prompt("Введите трехзначное число > ");
+Console.WriteLine(userNum);
+
+int result = userNum / 10 % 10;
+Console.WriteLine(result);
