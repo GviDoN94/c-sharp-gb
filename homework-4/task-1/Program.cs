6f9b0aee28764265ce736cc26238dde396322017
Vladislav Surin <vlad_surin@mail.ru>
2022-08-17 10:19:50 +0300

    finished second task

--- homework-4/task-1/Program.cs
+++ homework-4/task-1/Program.cs
@@ -1,4 +1,8 @@
-﻿int Prompt(string message)
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prompt(string message)
 {
     Console.Write(message);
     string readValue = Console.ReadLine();
---
+++ homework-4/task-2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+
+bool CheckUserNum(int userNum)
+{
+    return userNum > 0;
+}
+
+int SumDigitsInNumber(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+
+    return result;
+}
+
+int userNum = Prompt("Введите число > ");
+
+if (!CheckUserNum(userNum))
+{
+    System.Console.WriteLine("Нужно ввести положительное число");
+}
+else
+{
+    System.Console.WriteLine(SumDigitsInNumber(userNum));
+}
