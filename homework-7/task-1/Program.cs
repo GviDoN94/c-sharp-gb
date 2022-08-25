b6882a57373c075174ea027398175e780dd4aae9
Vladislav Surin <vlad_surin@mail.ru>
2022-08-25 22:33:13 +0300

    started do homework

---
+++ homework-7/task-1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n, заполненный случайными
+// вещественными числами.
+
+// m = 3, n = 4.
+// 0,5  7    -2   -0,2
+// 1   -3,3   8   -9,9
+// 8    7,8  -7,1  9
+
+int RandomPosNeg()
+{
+    int random = 1;
+    if (new Random().Next(0, 2) == 0)
+    {
+        return -random;
+    }
+    return random;
+}
+
+double[,] MultiArrayFill(int rows, int columns)
+{
+    double[,] multiArr = new double[rows, columns];
+    for (int i = 0; i < multiArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < multiArr.GetLength(1); j++)
+        {
+            double temp = new Random().NextDouble() * 10 * RandomPosNeg();
+            multiArr[i, j] = Math.Round(temp, 1);
+        }
+    }
+    return multiArr;
+}
+
+void PrintMultiArray(double[,] multiArr)
+{
+    for (int i = 0; i < multiArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < multiArr.GetLength(0); j++)
+        {
+            System.Console.Write(multiArr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+PrintMultiArray(MultiArrayFill(3, 4));
